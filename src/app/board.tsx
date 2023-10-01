a19cdb0780ee6201228a26e5170f0fda0bba788f
inker610566 <inker610566@gmail.com>
2023-10-01 14:43:17 +0800

    Refactor to use index in board.tsx

--- src/app/board.tsx
+++ src/app/board.tsx
@@ -5,66 +5,90 @@ import KomaUi from "./koma.tsx";
 import { Koma, getKomaRule } from "src/state/koma_rule.ts";
 import { KingKoma } from "src/state/koma.ts";
 import { Point } from "src/common/type.ts";
+import { comparePoint } from "src/common/util.ts";
 import { ROW_NUM, COL_NUM } from "src/common/constant.ts";
 import { useState } from "react";
+import { useImmer } from "use-immer";
 
 export default function Board() {
-  const [selectedKoma, setSelectedKoma] = useState<Point | undefined>(
-    undefined,
-  );
-  const [allKomas, setAllKomas] = useState<Koma[]>([]);
-  let nextMap: bool[][]|undefined = undefined;
+  const [selectedKomaIdx, setSelectedKomaIdx] = useState<number>(-1);
+  const [allKomas, setAllKomas] = useImmer<Koma[]>([
+    new KingKoma(0, { r: 3, c: 3 }),
+  ]);
+  const selectedKoma = allKomas[selectedKomaIdx];
+  let nextMap: bool[][] | undefined = undefined;
   if (selectedKoma) {
-      const rule = getKomaRule(selectedKoma);
-      nextMap = Array.from({length: ROW_NUM}).map(() => 
-          Array.from({length: ROW_NUM}).map(() => false)
-      );
-      for (const cell of rule.getMovablePoints(selectedKoma)) {
-          nextMap[cell.r][cell.c] = true;
-      }
+    const rule = getKomaRule(selectedKoma);
+    nextMap = Array.from({ length: ROW_NUM }).map(() =>
+      Array.from({ length: ROW_NUM }).map(() => false),
+    );
+    for (const cell of rule.getMovablePoints(selectedKoma)) {
+      nextMap[cell.r][cell.c] = true;
+    }
+    nextMap[selectedKoma.position.r][selectedKoma.position.c] = true;
   }
 
   function onClickCell(r: number, c: number) {
-    const clickedKoma = allKomas.find(
+    const clickedKomaIdx = allKomas.findIndex(
       ({ position: p }) => p.r === r && p.c === c,
     );
-    if (selectedKoma) {
-        if (nextMap[r][c]) {
-
-        }
-    } else {
-      if (clickedKoma) {
-        setSelectedKoma(clickedKoma);
+    if (selectedKomaIdx === -1) {
+      if (clickedKomaIdx) {
+        setSelectedKomaIdx(clickedKomaIdx);
       }
+      return;
+    }
+
+    if (r === selectedKoma.position.r && c === selectedKoma.position.c) {
+      setSelectedKomaIdx(-1);
+      return;
+    }
+
+    if (nextMap[r][c]) {
+      // Check if occupied.
+      setAllKomas((allKomas) => {
+        allKomas[selectedKomaIdx].position = { r, c };
+      });
+      setSelectedKomaIdx(-1);
     }
   }
 
-  function onClickKoma(koma: number) {
+  function onClickKoma(e: Event, komaIdx: number) {
+    // Judge player.
     if (!selectedKoma) {
-      setSelectedKoma(koma);
+      setSelectedKomaIdx(komaIdx);
+      return;
+    }
+    if (!comparePoint(selectedKoma.position, allKomas[komaIdx])) {
+      setSelectedKomaIdx(-1);
     }
   }
 
-  const koma = new KingKoma(0, { r: 3, c: 3 });
   return (
     <div
       className={["board", ...(selectedKoma ? ["select-koma"] : [])].join(" ")}
     >
-      {Array.from({ length: ROW_NUM }).map((_, i) => (
-        <div className="row" key={i}>
-          {Array.from({ length: COL_NUM }).map((_, j) => (
+      {Array.from({ length: ROW_NUM }).map((_, r) => (
+        <div className="row" key={r}>
+          {Array.from({ length: COL_NUM }).map((_, c) => (
             <div
-                className={[
-                    "cell",
-                    ...(nextMap?.[i]?.[j] ? ["next"] : [])
-                ].join(" ")}
-              key={`${i}_${j}`}
-              onClick={() => onClickCell(i, j)}
+              className={["cell", ...(nextMap?.[r]?.[c] ? ["next"] : [])].join(
+                " ",
+              )}
+              key={`${r}_${c}`}
+              data-key={`${r}_${c}`}
+              onClick={() => onClickCell(r, c)}
             ></div>
           ))}
         </div>
       ))}
-      {<KomaUi koma={koma} onClick={() => onClickKoma(koma)} />}
+      {allKomas.map((k, idx) => (
+        <KomaUi
+          koma={k}
+          key={`${k.position.r}_${k.position.c}`}
+          onClick={(e) => onClickKoma(e, idx)}
+        />
+      ))}
     </div>
   );
 }
--- src/common/util.ts
+++ src/common/util.ts
@@ -1,6 +1,16 @@
+import {Point} from "./type";
+
+
 export function castExists<T>(value: T|undefined): T {
     if (value === undefined) {
         throw new ValueError('Unexpected undefined');
     }
     return value;
+}
+
+export function comparePoint(p: Point, p2: Point): number {
+    if (p.r !== p2.r) {
+        return Math.sign(p.r - p2.r);
+    }
+    return Math.sign(p.c - p2.c);
 }
