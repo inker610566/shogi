8c018df1042a4e9459b2a83a9ed8ed1235fe5b24
inker610566 <inker610566@gmail.com>
2023-10-14 10:55:06 +0800

    Extract in turn logic into turn state type

--- src/app/board.tsx
+++ src/app/board.tsx
@@ -4,59 +4,99 @@ import "./board.css";
 import KomaUi from "./koma.tsx";
 import { getKomaRule } from "src/rule/koma.ts";
 import { Point } from "src/common/type.ts";
+import { Koma } from "src/rule/type.ts";
 import { BoardRule, InvalidMoveError } from "src/rule/board.ts";
-import { comparePoint, flatIterable } from "src/common/util.ts";
+import { comparePoint, flatIterable, assert } from "src/common/util.ts";
 import { ROW_NUM, COL_NUM } from "src/common/constant.ts";
 import { useState, useRef } from "react";
 import { useImmer } from "use-immer";
 
+enum TurnStateType {
+  OVERVIEW = "overview",
+  SELECTED_KOMA = "selected-koma",
+}
+
+/** The game state within a turn. */
+interface TurnState {
+  boardHighlight?: boolean[][];
+  handleClickCell(pos: Point): TurnChange | undefined;
+}
+
+interface TurnChange {
+  nextState: TurnState;
+  nextTurn?: boolean;
+}
+
+class OverviewState implements TurnState {
+  constructor(private readonly boardRule: BoardRule) {}
+
+  handleClickCell(pos: Point): TurnChange | undefined {
+    const koma = this.boardRule.board.getKoma(pos);
+    if (!koma) return undefined;
+    return { nextState: new SelectedKomaState(this.boardRule, koma) };
+  }
+}
+
+class SelectedKomaState implements TurnState {
+  boardHighlight?: boolean[][];
+  constructor(
+    private readonly boardRule: BoardRule,
+    private selectedKoma: Koma,
+  ) {
+    const { position } = selectedKoma;
+    assert(
+      this.boardRule.board.getKoma(position) === this.selectedKoma,
+      "Selected koma not belong to board",
+    );
+    this.boardHighlight = this.boardRule.getNextMap({ position });
+    // For click self cancel move.
+    this.boardHighlight![position.r][position.c] = true;
+  }
+
+  handleClickCell(pos: Point): TurnChange | undefined {
+    if (!comparePoint(this.selectedKoma.position, pos)) {
+      return { nextState: new OverviewState(this.boardRule) };
+    }
+    try {
+      this.boardRule.move(this.selectedKoma.position, pos);
+    } catch (e) {
+      if (!(e instanceof InvalidMoveError)) {
+        throw e;
+      }
+      return undefined;
+    }
+    return { nextState: new OverviewState(this.boardRule), nextTurn: true };
+  }
+}
+
 export default function Board() {
-  const [selectedKomaPos, setSelectedKomaPos] = useState<Point | undefined>(
-    undefined,
-  );
   const [turn, setTurn] = useState<number>(0);
   const boardRule = useRef<BoardRule | undefined>(undefined);
   if (!boardRule.current) {
     boardRule.current = new BoardRule();
     boardRule.current.addChangeListener(() => void setTurn(turn + 1));
   }
+  const [turnState, setTurnState] = useState<TurnState>(
+    new OverviewState(boardRule.current),
+  );
 
   function onClickCell(pos: Point) {
-    if (selectedKomaPos === undefined) {
-      const koma = boardRule.current!.board.getKoma(pos);
-      if (koma) {
-        // TODO: Check player.
-        setSelectedKomaPos(pos);
-      }
-      return;
+    const change = turnState.handleClickCell(pos);
+    if (!change) return;
+    const { nextState, nextTurn } = change;
+    if (nextState) {
+      setTurnState(nextState);
     }
-    if (!comparePoint(selectedKomaPos, pos)) {
-      setSelectedKomaPos(undefined);
-      return;
+    if (nextTurn) {
+      setTurn(turn + 1);
     }
-    try {
-      boardRule.current!.move(selectedKomaPos, pos);
-    } catch (e) {
-      if (!(e instanceof InvalidMoveError)) {
-        throw e;
-      }
-      return;
-    }
-    setTurn(turn + 1);
-    setSelectedKomaPos(undefined);
   }
 
-  const nextMap = selectedKomaPos
-    ? boardRule.current!.getNextMap({ position: selectedKomaPos })
-    : undefined;
-  if (selectedKomaPos) {
-    // For click self cancel move.
-    nextMap![selectedKomaPos.r][selectedKomaPos.c] = true;
-  }
+  const boardHighlight = turnState.boardHighlight;
 
   return (
     <div
-      className={["board", ...(selectedKomaPos ? ["select-koma"] : [])].join(
+      className={["board", ...(boardHighlight ? ["apply-highlight"] : [])].join(
         " ",
       )}
     >
@@ -64,9 +104,10 @@ export default function Board() {
         <div className="row" key={r}>
           {Array.from({ length: COL_NUM }).map((_, c) => (
             <div
-              className={["cell", ...(nextMap?.[r]?.[c] ? ["next"] : [])].join(
-                " ",
-              )}
+              className={[
+                "cell",
+                ...(turnState?.boardHighlight?.[r]?.[c] ? ["highlight"] : []),
+              ].join(" ")}
               key={`${r}_${c}`}
               data-key={`${r}_${c}`}
               onClick={() => void onClickCell({ r, c })}
